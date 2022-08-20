3c04ed221a74729f973b00f2745352c20f3cb4c0
Jin (he/him) <103966490+jinlikegold@users.noreply.github.com>
2022-08-19 18:07:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let homeScore = 0
+
+let homeScoreUpdate = document.getElementById("home-score")
+
+function plusOneHome() {
+    homeScoreUpdate.textContent = homeScore + 1
+    homeScore += 1
+}
+
+function plusTwoHome() {
+    homeScoreUpdate.textContent = homeScore + 2
+    homeScore += 2
+}
+
+function plusThreeHome() {
+    homeScoreUpdate.textContent = homeScore + 3
+    homeScore += 3
+} 
+
+let guestScore = 0
+
+let guestScoreUpdate = document.getElementById("guest-score")
+
+function plusOneGuest() {
+    guestScoreUpdate.textContent = guestScore + 1
+    homeScore += 1
+}
+
+function plusTwoGuest() {
+    guestScoreUpdate.textContent = guestScore + 2
+    guestScore += 2
+}
+
+function plusThreeGuest() {
+    guestScoreUpdate.textContent = guestScore + 3
+    guestScore += 3
+}
